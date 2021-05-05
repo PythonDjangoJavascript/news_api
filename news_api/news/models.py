ee136ed5d8b39f94f7669169140f2951b65070f9
Sayed <nuruddinsayeed@gmail.com>
2021-05-05 07:00:36 +0600

    Airticle model added

--- news_api/news/models.py
+++ news_api/news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    """Create database model for news articles"""
+
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
+    descirption = models.CharField(max_length=250)
+    body = models.TextField()
+    loacation = models.CharField(max_length=100)
+    publication_date = models.DateField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} by ({self.author})"

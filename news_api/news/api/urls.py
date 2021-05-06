dbfe4eaad2b0d68cd6930db18de7ddb8689a3930
Sayed <nuruddinsayeed@gmail.com>
2021-05-06 13:00:31 +0600

    class baseded article api added

--- news_api/news/api/urls.py
+++ news_api/news/api/urls.py
@@ -3,5 +3,8 @@ from news.api import views
 
 urlpatterns = [
     path('articles/', views.article_list_create_api_view, name='article-list'),
-    path('articles/<int:id>/', views.article_detail_api_view, name='article-detail')
+    path('articles/<int:id>/', views.article_detail_api_view, name='article-detail'),
+    path('articlescls/', views.ArticleListApiView.as_view(), name='articlecls-list'),
+    path('articlescls/<int:id>/', views.ArticleDetailApiView.as_view(),
+         name='articlecls-detail'),
 ]
--- news_api/news/api/views.py
+++ news_api/news/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from rest_framework.views import APIView
 
 from news.models import Airticle
 from .serializers import AirticleSerializer
@@ -54,5 +56,53 @@ def article_detail_api_view(request, id):
 
     elif request.method == 'DELETE':
         """Delete an article from the database"""
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ArticleListApiView(APIView):
+    """Classbased Api view that matian article list"""
+
+    def get(self, request):
+        """Mantain get request and return article list"""
+        articles = Airticle.objects.filter(active=True)
+        serializer = AirticleSerializer(articles, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        """Mantain post request and add article to the database"""
+        serializer = AirticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetailApiView(APIView):
+    """Classbased Api view mantain article detail response"""
+
+    def get_object(self, id):
+        """Fetch article object from database"""
+        return get_object_or_404(Airticle, pk=id)
+
+    def get(self, request, id):
+        """Fetch article object and return respnse"""
+        article = self.get_object(id)
+        serializer = AirticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        """Update article object"""
+        article = self.get_object(id)
+        serialzier = AirticleSerializer(article, data=request.data)
+        if serialzier.is_valid():
+            serialzier.save()
+            return Response(serialzier.data)
+        return Response(serialzier.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        """delete article from database"""
+        article = self.get_object(id)
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)

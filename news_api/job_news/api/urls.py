493f0ccc6dea577fe5a1fbeb03520ff2066bc12a
Sayed <nuruddinsayeed@gmail.com>
2021-05-09 12:00:16 +0600

    detail job offer api endpoint added

--- news_api/job_news/api/urls.py
+++ news_api/job_news/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('job_offers/', views.JobOfferApiView.as_view(), name='job-list'),
+    path('job_offers/<int:pk>/',
+         views.JobOfferDetailApiVew.as_view(), name='job-detail'),
 ]
--- news_api/job_news/api/views.py
+++ news_api/job_news/api/views.py
@@ -25,3 +25,32 @@ class JobOfferApiView(APIView):
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class JobOfferDetailApiVew(APIView):
+    """Return and edit Specific Joboffer response"""
+
+    def get_object(self, pk):
+        """Return Job offer object"""
+        return get_object_or_404(JobOffer, pk=pk)
+
+    def get(self, request, pk):
+        """Return Job Detail Response"""
+        serializer = JobOfferSerializer(self.get_object(pk), many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        """Update Job detail"""
+        serializer = JobOfferSerializer(
+            self.get_object(pk), data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """Delete Specific Job Offer"""
+        job_offer = self.get_object(pk)
+        job_offer.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
